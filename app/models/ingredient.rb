2408858bc0c320d8b53957d87e26354423c3f6a5
Ian Marshall <imarshall14@gmail.com>
2021-02-24 17:33:30 -0500

    commit tree 904f54042bb154a9bca94d2504a03fc8af7984c6

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,4 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 
-  def recipe_attributes=(recipe)
-    self.recipe = Recipe.find_or_create_by(title: recipe[:title])
-    self.recipe.update(recipe)
-  end
 end
